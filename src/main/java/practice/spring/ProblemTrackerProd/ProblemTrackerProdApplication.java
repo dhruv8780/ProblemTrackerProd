e4fc0c793eaabd09abfcccf8c8e8b900c07bebc1
Dhruvkumar Patel <pate2384@sheridancollege.ca>
2023-01-25 10:52:08 -0500

    Initial commit

---
+++ src/main/java/practice/spring/ProblemTrackerProd/ProblemTrackerProdApplication.java
@@ -0,0 +1,22 @@
+package practice.spring.ProblemTrackerProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ProblemTrackerProdApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "hello";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProblemTrackerProdApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/practice/spring/ProblemTrackerProd/ProblemTrackerProdApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring.ProblemTrackerProd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemTrackerProdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
